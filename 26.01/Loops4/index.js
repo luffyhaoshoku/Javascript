9ec27bcf9fcbca51916c40e7180d459cb44a3888
luffyhaoshoku <luffy3day2years@gmail.com>
2024-01-26 12:30:45 +0000

    while loop command . *= multiply and assign

--- 26.01/Loops4/index.js
+++ 26.01/Loops4/index.js
@@ -1,5 +1,16 @@
 
-
+/*i=0
 for (i; i<10; i++) {
-    console.log(i);
+    console.log(i); //10 iterations inclusing 0-9
+}*/
+
+let x=2;
+let loopCounter = 0;
+
+
+while (x < 10000) {
+    x *= x; //multiply upon itself and assign to the left value
+    loopCounter++;
+    console.log( x);
+    console.log(loopCounter);
 }
