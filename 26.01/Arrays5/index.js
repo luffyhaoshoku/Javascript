f8563a3abbff4caeeb701d2553d544ce7ab93fab
luffyhaoshoku <luffy3day2years@gmail.com>
2024-01-26 15:01:20 +0000

    creating a function to find words within sentences

--- 26.01/Arrays5/index.js
+++ 26.01/Arrays5/index.js
@@ -10,4 +10,33 @@ console.log(quote)
 quote.push("father");
 console.log(quote)
 
-quote.unshift("Luffy")
+quote.unshift("Luffy");
+console.log(quote);
+
+let erroneousWord = "Luffy"
+
+let LuffyIsHere = quote.find(n => {return n === erroneousWord});
+
+let LuffyIsAt;
+
+//LuffyIsAt = quote.findIndex(n => {return n === erroneousWord});
+
+
+if (LuffyIsHere){
+    LuffyIsAt = quote.findIndex(n => {return n === erroneousWord});
+    quote[LuffyIsAt] = "No";
+}
+
+let output = "";
+
+for (let i = 0, j = quote.length; i < j; i++) { 
+    if (i === j - 1) 
+    { output += quote[i] + '!'; } 
+    else if 
+    (quote[i] === 'No') { output += quote[i] + ', '; } 
+    else 
+    { output += quote[i] + ' ' } 
+}
+
+console.log(output);
+console.log("k")
