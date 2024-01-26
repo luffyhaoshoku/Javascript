36b42e9ad4d348156ea41f33c3e24446dc854801
luffyhaoshoku <luffy3day2years@gmail.com>
2024-01-26 11:40:08 +0000

    comepleted operators

--- 26.01/Operator2/index.js
+++ 26.01/Operator2/index.js
@@ -5,24 +5,25 @@ console.log(5 + 5);                       10
  console.log(5 + 10 / 2 * 5 - 10);         20
  console.log((6 + 10) / 2 * 5 - 10);        30
 
+ x = 0
 // Assignment Operators
-// console.log(x = x + 1);
-// console.log(x+= 1);
-// console.log(x++);
-// console.log(++x);
+console.log(x = x + 1);              1
+ console.log(x+= 1);                  //after 1 =2 
+ console.log(x++);                      //2+1 =3
+ console.log(++x);                    //4?
 
 // Relational Operators
-// console.log(5 > 3);
-// console.log(3 != 3);
-// console.log(3 <= 2 && 5 >2);
-// console.log(!5>3);
+ console.log(5 > 3); //t
+ console.log(3 != 3); //f
+ console.log(3 <= 2 && 5 >2); //f
+ console.log(!5>3); //t
 
 // Mismatched Types
-// console.log(5 + "5");
-// console.log(5 + true);
-// console.log(5 * "5");
-// console.log(1 == true);
-// console.log(1 === true);
+ console.log(5 + "5"); //joins word
+ console.log(5 + true); //true=1
+ console.log(5 * "5");  //25
+ console.log(1 == true); 
+ console.log(1 === true); //false
 
 
 
