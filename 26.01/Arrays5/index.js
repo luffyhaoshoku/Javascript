daef041d46f873f0ca65aa8a8140380ba5b946ee
luffyhaoshoku <luffy3day2years@gmail.com>
2024-01-26 14:17:58 +0000

    pop push ad unshift array

---
+++ 26.01/Arrays5/index.js
@@ -0,0 +1,13 @@
+
+let quote = ["I", "am", "your", "friend"];
+
+console.log(quote);
+console.log(quote[2])
+
+quote.pop();
+console.log(quote)
+
+quote.push("father");
+console.log(quote)
+
+quote.unshift("Luffy")
