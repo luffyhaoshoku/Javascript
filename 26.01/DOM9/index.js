a39333fb66bb6d910439df6d68dfba629dac39da
luffyhaoshoku <luffy3day2years@gmail.com>
2024-01-29 14:38:36 +0000

    created first function creating text element

--- 26.01/DOM9/index.js
+++ 26.01/DOM9/index.js
@@ -1,1 +1,35 @@
+/*document object model
+document access
+
+
+
+*/
+
+
 const colours = ['red', 'green', 'blue', 'orange', 'lime','olive', 'black', 'yellow'];
+
+
+function buildP (placeholder){
+
+    let p = document.createElement('p');
+    let text = document.createTextNode("have you tried turning it off and back on again?")
+
+
+    p.appendChild(text)
+    placeholder.appendChild(p) //div id =placeholder
+}
+
+buildP(document.querySelector('#placeholder'))
+//setting placeholder with element found replacing with div in html
+
+function buildP (placeholder, num){
+    let i = 0;
+    do{
+        
+
+
+
+
+    } while (i < num);
+
+}*/
