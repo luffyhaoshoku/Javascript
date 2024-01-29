6095c7dacf4a852a5732b0943d47761fe76bb835
luffyhaoshoku <luffy3day2years@gmail.com>
2024-01-29 14:50:54 +0000

    9 complete with math.random and fed in variable p

--- 26.01/DOM9/index.js
+++ 26.01/DOM9/index.js
@@ -9,7 +9,7 @@ document access
 const colours = ['red', 'green', 'blue', 'orange', 'lime','olive', 'black', 'yellow'];
 
 
-function buildP (placeholder){
+/*function buildP (placeholder){
 
     let p = document.createElement('p');
     let text = document.createTextNode("have you tried turning it off and back on again?")
@@ -20,16 +20,29 @@ function buildP (placeholder){
 }
 
 buildP(document.querySelector('#placeholder'))
-//setting placeholder with element found replacing with div in html
+//setting placeholder with element found replacing with div in html*/
 
 function buildP (placeholder, num){
     let i = 0;
     do{
-        
+        let p = document.createElement('p');
+    let text = document.createTextNode("have you tried turning it off and back on again?")
+
+    p.appendChild(text)
+    
+
+    p.style.color = colours[/*integer value*/parseInt(Math.random() * colours.length)];
+
+    placeholder.appendChild(p) //with num as second var. allows for loop of same elements
 
 
 
 
+
+
+        i++
     } while (i < num);
 
-}*/
+}
+
+buildP(document.querySelector('#placeholder'), 7);
