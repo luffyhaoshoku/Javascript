f2826e6b932575c6e2baaf092d0c49d71c656132
luffyhaoshoku <luffy3day2years@gmail.com>
2024-01-29 18:21:35 +0000

    half complete

--- 26.01/Manipulating10/index.js
+++ 26.01/Manipulating10/index.js
@@ -4,8 +4,25 @@ document.querySelector('button#bgColour').addEventListener('click', greenToPink)
 document.querySelector('button#fonts').addEventListener('click', tnrToArial);
 // Do not alter lines of code 2-4!
 
-function blueToRed() {}
+const blueParagraph = document.getElementById("blueParagraph")
 
+console.log(blueParagraph.style.color)//logging the colour of blue para
+
+console.log(getComputedStyle(blueParagraph).color); //colour obtained from getComputerStyle
+
+
+
+
+function blueToRed() { //button calls in function
+    blueParagraph.style.color = "red";
+    console.log(blueParagraph.style.color);
+    console.log(getComputedStyle(blueParagraph).color);
+    blueParagraph.textContent = blueParagraph.textContent.replace('blue', 'red');
+}
+
+const greenParagraphs = document.querySelectorAll(".greenBg");
+console.log(greenParagraphs[0].style.color); 
+console.log(getComputedStyle(greenParagraphs[0]).color);
 function greenToPink() {}
 
 function tnrToArial() {}
