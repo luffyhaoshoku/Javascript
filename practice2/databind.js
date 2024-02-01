64a02fb296bda579c171c54b851d214d249c4179
luffyhaoshoku <luffy3day2years@gmail.com>
2024-02-01 10:45:17 +0000

    :(

---
+++ practice2/databind.js
@@ -0,0 +1,55 @@
+if(data == undefined){
+    data= {};
+}
+
+let dataBind = function(){
+
+};
+
+dataBind.get = function(key){
+    return data[key];
+
+}
+
+
+databind.set = function(key,value){
+    data[key] = value;
+}
+
+
+
+databind.set = function(key,value){
+data[key] = value;
+}
+
+
+
+
+databind.display = function(){
+document.querySelectorAll('[name], [data]').forEach(function(item)){
+    if(item.name == undefined){
+        let key = item.getAttribute("data");
+        if(data[key] !== undefined){
+            item.innerText = data[key];
+        }
+    }
+    else{
+        let key = item.name;
+        if(data[key] !== undefined){
+            item.value = data[key];
+        }
+}};
+}
+
+dataBind.save = function(){
+console.log(JSON.stringify(data))
+sessionStorage.setItem('data', JSON.stringify(data))
+}
+
+
+dataBind.display();
+
+document.addEventListener("input", function(e){
+    dataBind.set(e.target.name, e.target.value);
+    dataBind.display();
+})
